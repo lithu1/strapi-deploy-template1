a1b49c6dd8b432df9e4bdcb4eea61db888df3455
LTHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-22 18:55:27 +0530

    Initial commit with Terraform and GitHub Actions

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "strapi" {
+  ami                    = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS
+  instance_type          = "t2.micro"
+  key_name               = "strapi-deploy-key"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              docker pull lithu213/strapi-app:${var.image_tag}
+              docker run -d -p 80:1337 lithu213/strapi-app:${var.image_tag}
+              EOF
+
+  tags = {
+    Name = "strapi-server"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.strapi.public_ip
+}
