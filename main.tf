87f1b012783d3a662ac5c52002878f6d86773aad
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 15:15:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,33 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_instance" "strapi_instance" {
+  ami                    = "ami-051f7e7f6c2f40dc1" # Amazon Linux 2 AMI (us-east-2)
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              curl -sL https://rpm.nodesource.com/setup_16.x | bash -
+              yum install -y nodejs git docker
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
+              docker run -d -p 1337:1337 lithu213/strapi-app:latest
+              EOF
+
+  tags = {
+    Name = "strapi-ec2"
+  }
+}
+
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg"
-  description = "Allow HTTP and SSH access"
+  description = "Allow SSH and HTTP access"
 
   ingress {
-    description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -17,9 +38,8 @@ resource "aws_security_group" "strapi_sg" {
   }
 
   ingress {
-    description = "HTTP access"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -30,24 +50,4 @@ resource "aws_security_group" "strapi_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_instance" "strapi_instance" {
-  ami                    = "ami-051f7e7f6c2f40dc1" # Amazon Linux 2 AMI in us-east-2
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
-  key_name               = var.key_name
-
-  tags = {
-    Name = "StrapiServer"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              docker run -d -p 80:1337 lithu213/strapi-app:${var.image_tag}
-            EOF
 }
