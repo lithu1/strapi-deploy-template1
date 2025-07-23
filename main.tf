1768eb5df7162dc3fc1934f72b769770069cfce8
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 11:40:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,66 @@
 provider "aws" {
-  region     = "us-east-2"
+  region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "strapi" {
-  ami                    = "ami-051f7e7f6c2f40dc1" # Amazon Linux 2023 in us-east-2
-  instance_type          = "t2.micro"
-  key_name               = "strapi-deploy-key"
+resource "aws_key_pair" "deployer" {
+  key_name   = "strapi-key"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Allow SSH and HTTP for Strapi"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Strapi"
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "strapi_ec2" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.strapi_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              amazon-linux-extras enable docker
-              yum install docker -y
+              yum install -y docker
               systemctl start docker
               systemctl enable docker
-              usermod -a -G docker ec2-user
-              docker pull lithu213/strapi-app:${var.image_tag}
-              docker run -d -p 80:1337 lithu213/strapi-app:${var.image_tag}
+
+              docker pull ${var.docker_image}
+              docker rm -f strapi-app || true
+              docker run -d -p 1337:1337 --name strapi-app ${var.docker_image}
               EOF
 
   tags = {
-    Name = "strapi-server"
+    Name = "Strapi-Server"
   }
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.strapi.public_ip
+output "strapi_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.strapi_ec2.public_ip
 }
