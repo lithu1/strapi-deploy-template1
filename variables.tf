872bd4194499648ef26b40c77e7b5ef756430340
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 11:28:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "image_tag" {}
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Docker image tag"
+  type        = string
+  default     = "latest"
+}
