c04cf9fc3a9edc27f597d8cbcf7501f20afd0b0e
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 11:36:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,17 @@ provider "aws" {
 }
 
 resource "aws_instance" "strapi" {
-  ami                    = "ami-0453898e98046c639" # Amazon Linux 2023
+  ami                    = "ami-051f7e7f6c2f40dc1" # Amazon Linux 2023 in us-east-2
   instance_type          = "t2.micro"
   key_name               = "strapi-deploy-key"
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
+              amazon-linux-extras enable docker
+              yum install docker -y
+              systemctl start docker
+              systemctl enable docker
               usermod -a -G docker ec2-user
               docker pull lithu213/strapi-app:${var.image_tag}
               docker run -d -p 80:1337 lithu213/strapi-app:${var.image_tag}
