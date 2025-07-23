c8f0b62a361f80e49770f4683da07fbf44968269
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 19:36:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,21 +21,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_ami" "amazon_linux2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "random_pet" "name" {
   length    = 2
   separator = "-"
@@ -52,10 +37,6 @@ resource "aws_security_group" "strapi_sg" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
   }
 
   ingress {
@@ -64,10 +45,6 @@ resource "aws_security_group" "strapi_sg" {
     to_port          = 1337
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
   }
 
   egress {
@@ -76,16 +53,13 @@ resource "aws_security_group" "strapi_sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
   }
 }
 
 resource "aws_instance" "strapi" {
-  ami                         = data.aws_ami.amazon_linux2.id
+  ami                         = "ami-0900fe555666598a2" # ✅ Amazon Linux 2 (us-east-2, 64-bit x86)
   instance_type               = "t2.micro"
+  key_name                    = "strapi-key12" # ✅ Use your existing key pair
   vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
   associate_public_ip_address = true
 
