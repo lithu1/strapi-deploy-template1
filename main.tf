add1d6a80a61caba52efcb27a0cf05410443ea03
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 11:27:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,83 +4,17 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-# Variables
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
-variable "image_tag" {
-  description = "Docker image tag"
-  type        = string
-  default     = "latest"
-}
-
-# Get latest Amazon Linux 2023 AMI
-data "aws_ami" "amazon_linux_2023" {
-  most_recent = true
-  owners      = ["137112412989"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
-# Security Group to allow HTTP
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
-  description = "Allow HTTP inbound traffic"
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 instance to run Strapi
 resource "aws_instance" "strapi" {
-  ami                    = data.aws_ami.amazon_linux_2023.id
+  ami                    = "ami-0453898e98046c639" # Amazon Linux 2023 AMI
   instance_type          = "t2.micro"
   key_name               = "strapi-deploy-key"
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install docker -y
-              systemctl start docker
-              systemctl enable docker
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
               docker pull lithu213/strapi-app:${var.image_tag}
               docker run -d -p 80:1337 lithu213/strapi-app:${var.image_tag}
               EOF
@@ -90,7 +24,6 @@ resource "aws_instance" "strapi" {
   }
 }
 
-# Output public IP
 output "ec2_public_ip" {
   value = aws_instance.strapi.public_ip
 }
