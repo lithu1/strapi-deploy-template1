1c42e43dd40a95d68cd57794bc624f68438a2b76
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 11:50:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
-  default = "us-east-2" # or your preferred region
+  default = "us-east-2"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  description = "Your AWS access key"
+}
 
-variable "public_key_path" {
-  description = "Path to your local public SSH key"
-  default     = "~/.ssh/id_rsa.pub"
+variable "aws_secret_key" {
+  description = "Your AWS secret key"
 }
 
 variable "docker_image" {
-  description = "Docker image to deploy (e.g. lithu213/strapi-app:latest)"
+  description = "e.g. lithu213/strapi-app:latest"
   type        = string
 }
