75d881621823f2c33e7bce11e72ff18d7401b53d
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 12:19:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
-  sensitive   = true
 }
 
 variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
-  sensitive   = true
 }
 
 variable "image_tag" {
-  description = "Docker image tag (unused but passed via CLI)"
+  description = "Tag of the Docker image to deploy"
   type        = string
   default     = "latest"
+}
+
+variable "public_key_path" {
+  description = "Path to the SSH public key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }
