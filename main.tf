c292ccd856669af593d77210a02d5f3793622759
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-23 15:29:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,21 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-# Use this key to connect to EC2 (copied from your local machine or injected via CI)
+# Generate unique suffix
+resource "random_pet" "suffix" {}
+
+# Use dynamic name for key pair
 resource "aws_key_pair" "strapi_key" {
-  key_name   = "strapi-deploy-key"
+  key_name   = "strapi-deploy-key-${random_pet.suffix.id}"
   public_key = <<-EOT
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEy7mRA4VsQ57ukPnu3ixDyTddcG2SV9t6gWuUaIlG2MtTxxkmzoX4CTZP+ucrz/T0R/AzMhz4/eDbpJOddrrlasUpbkUik2RyqkHkcOhmRha4K4+fRq+pYYCI+I5iaqU/DlvzNxmU3gqmUmsRvACXbzLJL2QBUjx1/7U/BUUvze8JWtEEsvOrULxke5b/U2r/cxAf+SvMgls49du7ac5zz+v2FTYyhjfOirpmok/vweBj8ehujRnEpizcnPsGkGa2V7TQRGnfWiIbwAI583doVFpDr84SfmMFH46AqDuXiRd0qnPLFCcc/cHXY1z5QSuwMzXhuKb61tqe4OXovM+o1IJ2/3GdrVjlTBX7hYBOspAVPKbK3RlGL9Z2+jhDKGlVfspg2IDsrndxLs5jsymYI/uHRWKwXvtyeZkFm98/BrJ7C9Lk0EKZAJFyYTRhgrp75PogqT601f5W8J3QY61VXVp3pc8+jO7G+2Mfbc41DGpL7qXQ0mCYd/r/qE4XQiTtF+cxIrj0fOk6YnnoqQhbnu9F9/JlDSHl2DVGoBeOaGqNL9/n2I2BV8CqvgclGT+AbxEuzn/ceA6xZrcjHHAFxAE40CriEwRSqaxvvQsg0VidxWkgNxd6ODjGRFVQ81in2A3IYGBUh+8ZEEdSweEe9Frk3xipSo2UuBBuQ3wKjw== root@DESKTOP-JQV1A3N
   EOT
 }
 
-# Create a security group to allow Strapi (port 1337) and SSH (port 22)
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "strapi_sg" {
   name_prefix = "strapi-sg-"
   description = "Allow inbound traffic for Strapi"
@@ -23,7 +29,7 @@ resource "aws_security_group" "strapi_sg" {
     to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow HTTP for Strapi"
+    description = "Allow Strapi"
   }
 
   ingress {
@@ -42,22 +48,12 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
-# Get default VPC for security group
-data "aws_vpc" "default" {
-  default = true
-}
-
-# EC2 Instance running Strapi via Docker
 resource "aws_instance" "strapi" {
   ami                         = "ami-0c55b159cbfafe1f0"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.strapi_key.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
-
-  tags = {
-    Name = "strapi-server"
-  }
 
   user_data = <<-EOT
               #!/bin/bash
@@ -67,14 +63,16 @@ resource "aws_instance" "strapi" {
               systemctl start docker
               systemctl enable docker
               usermod -a -G docker ec2-user
-
               docker pull lithu213/strapi-app:latest
               docker rm -f strapi-app || true
               docker run -d -p 1337:1337 --name strapi-app lithu213/strapi-app:latest
             EOT
+
+  tags = {
+    Name = "strapi-server"
+  }
 }
 
-# Output public IP
 output "ec2_public_ip" {
   value = aws_instance.strapi.public_ip
 }
